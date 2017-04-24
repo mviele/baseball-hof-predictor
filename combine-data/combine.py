ef57e2080d930860868098c37d038d2c235a2cba
Matt Viele <mviele@utexas.edu>
2017-04-24 16:22:57 -0500

    fixed ERA, and dropped BAOpp because no reliable way to calculate it over a career exists within this data

--- combine-data/combine.py
+++ combine-data/combine.py
@@ -6,9 +6,13 @@ import pandas as pd
 def main():
     df_init = pd.read_csv('Pitching.csv', header=0)
 
-    df = df_init.drop(['yearID', 'stint', 'teamID', 'lgID'], axis=1, inplace=False)
+    df = df_init.drop(['yearID', 'stint', 'teamID', 'lgID', 'ERA', 'BAOpp'], axis=1, inplace=False)
     df = df.set_index('playerID')
     df = df.groupby(df.index).sum()
+    df['ERA'] = (df['ER'] * 27) / df['IPouts']
+    print(df)
     df.to_csv("combined_stats.csv")
+
+
 
 main()
