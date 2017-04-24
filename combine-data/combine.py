8904b74f18ca57aa429ee1d23a0f8cc5b556a784
Matt Viele <mviele@utexas.edu>
2017-04-24 14:17:44 -0500

    First attempt at combining data by player, this takes a long time to run

--- combine-data/combine.py
+++ combine-data/combine.py
@@ -1,1 +1,19 @@
 #Python 3
+
+import pandas as pd
+
+
+def main():
+    df_init = pd.read_csv('Pitching.csv', sep=',', header=0)
+    
+    df = df_init.loc[df_init['yearID'] == 1871]
+    df.drop(['yearID', 'stint', 'teamID', 'lgID'], axis=1, inplace=False)
+
+    for year in range(1872, 2016):
+        df_new = df_init.loc[df_init['yearID'] == year]
+        df_new.drop(['yearID', 'stint', 'teamID', 'lgID'], axis=1, inplace=False)
+        df = pd.concat([df, df_new], axis=1)
+    
+    print(df)
+
+main()
