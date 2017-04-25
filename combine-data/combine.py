ccfab7355dfe65f44adda241cde7e42f9d564fc3
Matt Viele <mviele@utexas.edu>
2017-04-25 16:06:27 -0500

    More data massaging, added whether a player is a hall of famer now

--- combine-data/combine.py
+++ combine-data/combine.py
@@ -1,6 +1,7 @@
 #Python 3
 
 import pandas as pd
+import numpy as np
 
 
 def main():
@@ -9,9 +10,23 @@ def main():
     df = df_init.drop(['yearID', 'stint', 'teamID', 'lgID', 'ERA', 'BAOpp'], axis=1, inplace=False)
     df = df.set_index('playerID')
     df = df.groupby(df.index).sum()
+    df = df[df['G'] >= 50]
     df['ERA'] = (df['ER'] * 27) / df['IPouts']
     print(df)
-    df.to_csv("combined_stats.csv")
+
+    df_row_count = df.shape[0]
+    df['HOF'] = np.zeros((df_row_count,), dtype=np.int)
+    print(df)
+    df_hof = pd.read_csv('HallOfFame.csv', header=0)
+    df_mem = df_hof.loc[df_hof['inducted'] == 'Y']
+    print(df_mem)
+
+    for x in df_mem['playerID']:
+        if x in df.index:
+            df.set_value(x, 'HOF', 1)
+    
+    print(df.loc[df['HOF'] == 1])
+    # df.to_csv("combined_stats.csv")
 
 
 
