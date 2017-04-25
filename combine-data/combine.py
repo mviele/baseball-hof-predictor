93fd70bcc21064c7b93df1a3bb5facaff82c39b6
Matt Viele <mviele@utexas.edu>
2017-04-25 16:36:49 -0500

    Added all-star appearances column to data, and left that column as NaN if a player retired in or before 1938 (5 years after the first all-star game)

--- combine-data/combine.py
+++ combine-data/combine.py
@@ -12,21 +12,34 @@ def main():
     df = df.groupby(df.index).sum()
     df = df[df['G'] >= 50]
     df['ERA'] = (df['ER'] * 27) / df['IPouts']
-    print(df)
+
 
     df_row_count = df.shape[0]
     df['HOF'] = np.zeros((df_row_count,), dtype=np.int)
-    print(df)
+    df['AS'] = np.zeros((df_row_count,), dtype=np.int)
+
     df_hof = pd.read_csv('HallOfFame.csv', header=0)
     df_mem = df_hof.loc[df_hof['inducted'] == 'Y']
-    print(df_mem)
 
     for x in df_mem['playerID']:
         if x in df.index:
-            df.set_value(x, 'HOF', 1)
+            df.ix[x, 'HOF'] = 1
     
+
+    df_as = pd.read_csv('AllstarFull.csv', header=0)
+    df_as = df_as.set_index('playerID')
+    for p in df_as.index:
+        if p in df.index:
+            df.ix[p, 'AS'] += 1
+            
+    df_master = pd.read_csv('Master.csv', header=0)
+    df_master = df_master.set_index('playerID')
+    for p in df.index:
+        if int(df_master.ix[p, 'finalGame'][:4]) <= 1938:
+            df.ix[p, 'AS'] = np.nan
+
     print(df.loc[df['HOF'] == 1])
-    # df.to_csv("combined_stats.csv")
+    df.to_csv("combined_stats.csv")
 
 
 
